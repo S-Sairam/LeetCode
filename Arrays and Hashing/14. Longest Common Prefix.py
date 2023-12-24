452787c4de1f1822202c8851ce5bf13f8478dcac
Sairam S <125436937+S-Sairam@users.noreply.github.com>
2023-12-24 08:07:49 +0530

    Create 14. Longest Common Prefix.py

---
+++ Arrays and Hashing/14. Longest Common Prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result=''
+        if len(strs)==1:
+            return strs[0]
+        strs = sorted(strs)
+        mini=strs[0]
+        maxi=strs[-1]
+        for i in range(len(strs[0])):
+            if mini[:i+1]==maxi[:i+1]:
+                result = maxi[0:i+1]
+        return result
